a4672d0f2a4d873c63c732ec0fdb696b277b5870
GiftXXVI <gift.chimphonda@outlook.com>
2022-02-08 18:16:13 +0200

    Updated Code Documentation

--- student_code.py
+++ student_code.py
@@ -19,6 +19,44 @@ def distance(origin, destination) -> float:
     delta_y = (origin[1] - destination[1])**2
     return math.sqrt(delta_x + delta_y)
 
+
+# From http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html.
+# Accessed 8 February 2022.
+
+'''
+This implementation of the A* Algorithm uses the Euclidean Distance
+as its heuristic function.
+
+Other options include:
+
+1. The Manhattan Distance: this can be used in a grid of squares where each
+location is a square and movement is restricted to the 4 sides of the
+square.
+
+def m_distance(origin,destination) -> int:
+    delta_x = abs(origin[0] - destination[0])
+    delta_y = abs(origin[0] - destination[0])
+    return delta_x + delta_y
+
+2. The Diagonal Distance: this can be used in a grid of squares where each
+location is a square but movement is permitted to the 4 sides of the
+square as well as the 4 diagonals of the square.
+
+cost_n is the cost of a normal step, cost_d is the cost of a diagonal step.
+
+The diagonal step enables one to avoid taking 2 normal steps, therefore this
+is taken into account in the return statement below in the expession:
+(cost_d - 2 * cost_n)
+
+def d_distance(origin, destination) -> int:
+    cost_n = 1
+    cost_d = 1
+    delta_x = abs(origin[0] - destination[0])
+    delta_y = abs(origin[1] - destination[1])
+    return cost_n * (delta_x + delta_y) + (cost_d - 2 * cost_n) \
+    * min(delta_x, delta_y)
+'''
+
 # From https://en.wikipedia.org/wiki/A*_search_algorithm. Accessed 7
 # February 2022.
 
